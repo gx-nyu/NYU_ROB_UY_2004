e04ecb172cfef5f90072255709fcd1a96308bed2
gx-nyu <gx2062@nyu.edu>
2026-02-22 15:55:07 -0500

    created a2

---
+++ Assignments/Assignment_2/my_assignment_2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def get_T01(theta_1):
+    matrix = np.array([],[],[],[0,0,0,1])
+    return
+
+def get_T12(theta_2):
+    return
+
+def get_T23(theta_3):
+    return
+
+def get_T34():
+    return
+
+
+def ee_in_collision(angles_list,p_point,tolerance):
+    
+    return False
